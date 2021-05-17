aaf67b4f1c96b15f8b44b717ee2f8aa9ad8b44de
hugomrmartinsa210701372 <hugomrmartins.info@gmail.com>
2021-05-17 21:12:17 +0100

    Camera now follows player

---
+++ Assets/Scripts/PlayerStuff/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothSpeed;
+    [SerializeField] private Vector3 offset;
+
+    // FixedUpdate is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 finalPos = target.position + offset;
+        Vector2 smoothPos = Vector3.Lerp(transform.position, finalPos, smoothSpeed);
+        transform.position = smoothPos;
+        
+
+        transform.LookAt(smoothPos);
+    }
+}
