eb0a77c23bc14c9d3a588432122f7e27a9848f70
hugomrmartinsa210701372 <hugomrmartins.info@gmail.com>
2021-05-17 18:11:35 +0100

    Created new branch
    Added door teleport

---
+++ Assets/moveToDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveToDoor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject character;
+    [SerializeField] private Transform moveTo;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(character.tag == "Player")
+            character.transform.position = moveTo.transform.position;
+    }
+}
