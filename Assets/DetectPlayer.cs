6911201577f9eee0cac77b60d120f04f9a79e670
hugomrmartinsa210701372 <hugomrmartins.info@gmail.com>
2021-05-18 07:00:31 +0100

    State machine and chase
    would you look at that, they move, gotta make ya boi jake patrol

---
+++ Assets/DetectPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour
+{
+    [SerializeField] private float maxRange;
+
+    [SerializeField] private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.queriesStartInColliders = false;
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //raycast range
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, maxRange);
+
+        //check if found player
+        if(hitInfo.collider != null)
+        {
+            if (hitInfo.collider.CompareTag("Player")) 
+            {
+                anim.SetBool("isChasing", true);
+                Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+
+            }
+            
+        }
+        else
+            Debug.DrawLine(transform.position, transform.position + transform.right * maxRange, Color.green);
+    }
+}
